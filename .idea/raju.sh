b9b002061a0fc43602267003a6b4b75405b43d3a
vasu <bandaruvasu1@gmail.com>
2022-01-21 11:29:20 +0530

    test file for practice

---
+++ .idea/raju.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo vasu
+
+echo -e "\e[32m what is your name"
+
+echo -e "my name is vasu\e[0m"
+
+echo -e "vasu\t\traju"
+
+echo -e "mother\nfather"
+
+a=100
+b=200
+
+echo -e "the value of a =$a"
+echo -e "the value of b =$b"
