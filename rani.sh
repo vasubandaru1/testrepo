09a265af19bc13bac8b7ba889a5ac19313e24a41
vasu <bandaruvasu1@gmail.com>
2022-01-21 12:59:58 +0530

    test file for practice

--- rani.sh
+++ rani.sh
@@ -7,3 +7,9 @@ echo ${x}x${y}=200
 
 date=$date
 echo -e "to day date is $(date +%F)"
+
+reade -p "what is your name: " name
+
+read -p "what is your age: "   age
+
+echo -e "your name = $name\nyour age = $age"
