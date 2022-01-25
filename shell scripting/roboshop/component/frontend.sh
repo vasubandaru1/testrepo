6ea3b5b9db01b1b200e9250f542fa99815cc1946
vasu <bandaruvasu1@gmail.com>
2022-01-25 22:47:46 +0530

    frontend

--- shell scripting/roboshop/component/frontend.sh
+++ shell scripting/roboshop/component/frontend.sh
@@ -2,13 +2,13 @@
 
 
 print() {
-  echo -n -e "\e[1m$1\e[0m.."
-  echo -e "\n\e[36m............. $1 .............\e[0m" >>$LOG
+  echo -n -e '\e[1m$1\e[0m..'
+  echo -e '\n\e[36m............. $1 .............\e[0m' >>$LOG
 }
 
 stat() {
-  if [ $1 -eq 0]; then
-    echo -e "\e[1;32mSUCESS\e[0m"
+  if [ $1 -eq 0 ]; then
+    echo -e '\e[1;32mSUCESS\e[0m'
    else
     echo -e "\e[1;33mFAILURE\e[0m"
      echo -e "\e[1;34mscript the failed and check detailed log in $LOG file \e[0m"
