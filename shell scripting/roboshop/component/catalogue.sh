9a2c3fe37f7471aac4e4e67a8609d31f2a3b100d
vasu <bandaruvasu1@gmail.com>
2022-01-26 15:12:11 +0530

    Catalogue

--- shell scripting/roboshop/component/catalogue.sh
+++ shell scripting/roboshop/component/catalogue.sh
@@ -7,3 +7,34 @@ yum install -y gcc-c++ make &>>$LOG
 curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash - &>>$LOG
 yum install -y nodejs &>>$LOG
 stat $?
+
+print "Add roboshop user"
+id roboshop &>>$LOG
+if [ $? -eq 0 ]; then
+  echo user Roboshop already exists &>>$LOG
+else
+  useradd roboshop &>>$LOG
+fi
+stat $?
+
+print "Download catalogue"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>$LOG
+stat $?
+
+print "Remove old content"
+rm -rf /home/roboshop/catalogue
+stat $?
+
+print "Extract catalogue"
+unzip -o -d /home/roboshop /tmp/catalogue.zip &>>$LOG
+stat $?
+
+print "copy content"
+mv /home/roboshop/catalogue-main /home/roboshop/catalogue
+stat $?
+
+print "Nodejs dependecies"
+npm install --unsafe-perm &>>$LOG
+stat $?
+
+
