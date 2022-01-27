ba43dd6e3bd0d92a2677547c340d255647e89d6e
vasu <bandaruvasu1@gmail.com>
2022-01-27 15:25:04 +0530

    catalogue

--- shell scripting/roboshop/component/create-instance.sh
+++ shell scripting/roboshop/component/create-instance.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-count=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpaddress" | grep -v null | wc -l)
-if [ $count -eq 0 ]; then
-aws ec2 run-instances --image-id ami-0eb5f3f64b10d3e0e --instance-type t3.micro --security-group-ids sg-0a57500c7aa97fd16 --tag-specifications "ResourceType=instance,Tags=[{key=Name,value=$1}]" | jq
-else
-   echo "Instance already exists"
- fi
+aws ec2 run-instances --image-id ami-04656078adf4aa403 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
