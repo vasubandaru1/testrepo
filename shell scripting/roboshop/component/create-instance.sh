8547546f628900c52110567326531a8aa61c0df2
vasu <bandaruvasu1@gmail.com>
2022-01-27 16:01:53 +0530

    catalogue

--- shell scripting/roboshop/component/create-instance.sh
+++ shell scripting/roboshop/component/create-instance.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-aws ec2 run-instances --image-id ami-04656078adf4aa403 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
+ count=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].InstanceId" | wc -l)
+
+ if [ $1 -eq 0 ]; then
+   aws ec2 run-instances --image-id ami-04656078adf4aa403 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+   else
+     echo "Instance already exists"
+
+fi
