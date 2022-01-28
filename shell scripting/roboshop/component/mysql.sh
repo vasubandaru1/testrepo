d730859aba6c19b0377156307ee677ac4a45d690
vasu <bandaruvasu1@gmail.com>
2022-01-28 17:24:25 +0530

    catalogue

--- shell scripting/roboshop/component/mysql.sh
+++ shell scripting/roboshop/component/mysql.sh
@@ -17,7 +17,8 @@ print "Start mysql sevice"
 systemctl enable mysqld &>>$LOG && systemctl start mysqld &>>$LOG
 stat $?
 
-DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}') NEW_PASSWORD='Roboshop@1'
+DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+NEW_PASSWORD='Roboshop@1'
 
 echo 'show databases;' | mysql -uroot -p'${NEW_PASSWORD}' &>>$LOG
 if [ $? -ne 0 ]; then
