e5ac44f8163f8f8b7c8cdbc6e92b187a37316b08
vasu <bandaruvasu1@gmail.com>
2022-01-29 20:05:22 +0530

    catalogue

--- shell scripting/roboshop/component/rabbitmq.sh
+++ shell scripting/roboshop/component/rabbitmq.sh
@@ -7,7 +7,7 @@ COMPONENT=rabbitmq
 
 print "Install erlang"
 yum list installed | grep erlang &>>$LOG
-if [ $1 -ne 0 ]; then
+if [ $? -ne 0 ]; then
  yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
  fi
  stat $?
