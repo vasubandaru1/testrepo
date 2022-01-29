12814a153397d23ad62d4134196c7ba3c2b4bf0c
vasu <bandaruvasu1@gmail.com>
2022-01-29 22:50:38 +0530

    catalogue

--- shell scripting/roboshop/component/common.sh
+++ shell scripting/roboshop/component/common.sh
@@ -36,7 +36,7 @@ SYSTEMD() {
   stat $?
 
   print "update DNS records in systemD config"
-  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/" -e "s/CARTENDPOINT/cart.roboshop.internal/" -e "s/DBHOST/mysql.roboshop.internal/" /home/roboshop/$COMPONENT/systemd.service &>>$LOG
+  sed -i -e "s/MONGO_DNSNAME/mongodb.roboshop.internal/" -e "s/REDIS_ENDPOINT/redis.roboshop.internal/" -e "s/MONGO_ENDPOINT/mongodb.roboshop.internal/"-e "s/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/" -e "s/CARTENDPOINT/cart.roboshop.internal/" -e "s/DBHOST/mysql.roboshop.internal/" -e "s/CARTHOST/cart.roboshop.internal/" -e "s/USERHOST/user.roboshop.internal/" -e "s/AMQHOST/rabbitmq.roboshop.internal/" /home/roboshop/$COMPONENT/systemd.service &>>$LOG
   stat $?
 
   print "copy systemd file"
